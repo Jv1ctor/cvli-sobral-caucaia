5cba0c651c415ae93f318b1322a203be84edd515
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-03-13 17:43:38 -0300

    restore config.R

---
+++ config.R
@@ -0,0 +1,23 @@
+library(readxl)
+base <- read_excel("base.xlsx")
+View(base)
+
+meses_pt <- c("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
+              "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+df <- data.frame(base)
+
+# derive date/time fields (year, month, hour) for plotting
+
+df$Data <- as.Date(df$Data)
+df <- na.omit(df)
+df$Ano <- format(df$Data, "%Y")
+
+idx_mes <- as.numeric(format(df$Data, "%m"))
+
+df$Mes_Num <- idx_mes
+df$Mes <- meses_pt[idx_mes]
+
+
+hora_formatada <- as.POSIXct(df$Hora, format="%H:%M")
+
+df$Hora <- hora_formatada
