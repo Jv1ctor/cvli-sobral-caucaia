1f28f487963e33d1705fd4802c32043b0663bad7
nicolas castro <nicribeirolis@gmail.com>
2026-03-17 15:26:36 -0300

    Update bar_chart.R

--- bar_chart.R
+++ bar_chart.R
@@ -1,15 +1,19 @@
 source("config.R")
 
-cont_dia <- table(df$Dia_Semana)
-barplot(cont_dia, 
-        main = "Ocorrências por Dia da Semana", 
-        col = "orange", 
-        las = 2, 
+# Ajuste do nome da coluna conforme a base da SSPDS
+cont_dia <- table(df$Dia_da_Semana) 
+
+barplot(cont_dia,
+        main = "Ocorrências por Dia da Semana",
+        col = "orange",
+        las = 2,
         cex.names = 0.8)
 
-cont_esc <- table(df$Escolaridade_da_Vitima)
-barplot(cont_esc, 
-        main = "Escolaridade das Vítimas", 
-        col = "darkgreen", 
-        las = 2, 
+# Ajuste da escolaridade (geralmente é Escolaridade_Vitima ou Escolaridade)
+cont_esc <- table(df$Escolaridade_Vitima)
+
+barplot(cont_esc,
+        main = "Escolaridade das Vítimas",
+        col = "darkgreen",
+        las = 2,
         cex.names = 0.7)
